0c9dec5bb3b3f0bd82873974c6850cf87b19acb9
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-01-12 12:30:37 +0300

    Update №14.5 Лут в рейде.py

--- №14.5 Лут в рейде.py
+++ №14.5 Лут в рейде.py
@@ -1,1 +1,36 @@
-asd
+num_input = input('Введите количество предметов: ')
+items_input = input('Введите ценность каждого предмета через пробел: ')
+
+try:
+    num = int(num_input)
+    items = list(map(int, items_input.split()))
+
+    sorted_items = items.copy()
+
+    i = 0
+    while i < len(sorted_items) - 1:
+        j = 0
+        while j < len(sorted_items) - i - 1:
+            if sorted_items[j] > sorted_items[j + 1]:
+                temp = sorted_items[j]
+                sorted_items[j] = sorted_items[j + 1]
+                sorted_items[j + 1] = temp
+            j += 1
+        i += 1
+
+    selected_items = []
+    index = 0
+    while index < num and index < len(sorted_items):
+        selected_items.append(sorted_items[index])
+        index += 1
+
+    total_value = 0
+    k = 0
+    while k < len(selected_items):
+        total_value += selected_items[k]
+        k += 1
+
+    print(f'Максимальная возможная суммарная ценность: {total_value}')
+
+except ValueError:
+    print('Ошибка ввода данных. Убедитесь, что вводите целые числа.')
