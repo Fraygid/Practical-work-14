4c8f9e709a7d6938258305b73fcb49d932b30686
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-04-22 09:36:57 +0300

    Update №14.7 Bubble sort.py

--- №14.7 Bubble sort.py
+++ №14.7 Bubble sort.py
@@ -1,1 +1,21 @@
-ads
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+print(f'Исходный список: {numbers}')
+
+sorted_numbers = numbers.copy()
+num = len(sorted_numbers)
+swapped = True
+
+while swapped:
+    swapped = False
+    i = 0
+    while i < num - 1:
+        if sorted_numbers[i] > sorted_numbers[i + 1]:
+            temp = sorted_numbers[i]
+            sorted_numbers[i] = sorted_numbers[i + 1]
+            sorted_numbers[i + 1] = temp
+            swapped = True
+        i += 1
+    num -= 1
+
+print(f'Отсортированный список: {sorted_numbers}')
